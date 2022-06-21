2af85d4842f62037f6bb621f76d14d7973c66633
Megan Pellow <56059002+pellowm@users.noreply.github.com>
2022-06-21 15:36:51 -0400

    fixing button class

--- client/src/views/Game.js
+++ client/src/views/Game.js
@@ -108,7 +108,7 @@ class Game extends React.Component {
                             <RankCard {...this.state.currentGame} />
                                 {this.state.gameInProgress === true && this.state.questionIndex > 0 && <button className="btn btn-outline-success m-3" onClick={this.onClickPrev}>Prev</button>}
                                 {this.state.gameInProgress === true && this.state.questionIndex < this.state.currentGame.questions.length-1 && <button className="btn btn-outline-success" onClick={this.onClickNext}>Next</button>}
-                                {this.state.gameInProgress === true && this.state.questionIndex === this.state.currentGame.questions.length-1 && this.state.showSubmitButton === true && <button onClick={this.onClickSubmit}>Submit</button>}
+                                {this.state.gameInProgress === true && this.state.questionIndex === this.state.currentGame.questions.length-1 && this.state.showSubmitButton === true && <button className="btn btn-outline-success" onClick={this.onClickSubmit}>Submit</button>}
                                 {this.state.gameInProgress === false && <Link  className="nav-link" to="/"><button className="btn btn-outline-success" type="button">New Game</button></Link>}
                             </div>
                         </ErrorBoundary>
